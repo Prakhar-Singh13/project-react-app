b475d15e53d9b9e4e8fd1762b4ab71260f1617d2
Prakhar Singh <prakharsingh13.ps@gmail.com>
2021-05-29 20:57:23 +0530

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './Home';
+import Header from './Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the Project!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org/docs/create-a-new-react-app.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React App
-        </a>
-      </header>
+
+    // BEM
+    <div className="app">
+      <Header />
+      <Home />
+
+      
+    {/* Home */}
+          {/* Header */}
+
+          {/* Banner */}
+              {/* Search */}
+
+          {/* Cards */}
+
+          {/* Footer */}
+
+    {/* SeachPage */}
+        {/* Header */}
+        {/* ... */}
+
     </div>
   );
 }
