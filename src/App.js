3995bfd819e548c6cefb296a79d69f769b4af5e6
Prakhar Singh <prakharsingh13.ps@gmail.com>
2021-05-31 04:56:45 +0530

    Initial Files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 
+import React from 'react';
 import './App.css';
-import Home from './Home';
-import Header from './Header';
+import Home from './Home'
+import Header from './Header'
+import Footer from './Footer'
+import SearchPage from './SearchPage'
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
 
     // BEM
     <div className="app">
-      <Header />
-      <Home />
-
-      
-    {/* Home */}
-          {/* Header */}
-
-          {/* Banner */}
-              {/* Search */}
-
-          {/* Cards */}
-
-          {/* Footer */}
-
-    {/* SeachPage */}
-        {/* Header */}
-        {/* ... */}
-
+      <Router>
+        <Header />
+        
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+        <Footer />
+      </ Router>
     </div>
   );
 }
